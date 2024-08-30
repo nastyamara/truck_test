8374a00da0fbb293bca0b56132ee15cab05e7229
nastyamara <mashamarmar@gmail.com>
2024-08-30 16:18:20 +0300

    settings added

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
+
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Routes>
+        
+</Routes>
+    </div>
+  )
+}
+
+export default App
